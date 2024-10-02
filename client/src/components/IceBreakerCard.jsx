f3814b8b76f83c21fc26266aefbdebb5373a1b62
valyastriz <valyastriz@gmail.com>
2024-10-01 23:17:39 -0400

    added horizontal white line on card component to divide title from text

--- client/src/components/IceBreakerCard.jsx
+++ client/src/components/IceBreakerCard.jsx
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent, Typography, Divider } from '@mui/material';
 
 const IceBreakerCard = ({ title, description }) => (
     <Card sx={{ minWidth: 275, maxWidth: 300, margin: 2 }}>
@@ -6,6 +6,13 @@ const IceBreakerCard = ({ title, description }) => (
             <Typography variant="h5" component="div">
                 {title}
             </Typography>
+            <Divider sx={{ 
+                my: 1,
+                borderBottomWidth: 1,
+                width: '100%',
+                mx: 'auto',
+                borderColor: 'white'
+            }} />
             <Typography variant="body2">
                 {description}
             </Typography>
