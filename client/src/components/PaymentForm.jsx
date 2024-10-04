cf1391b3baf36a48812164f121d36e4c8ea0831d
Ashley Wright <ashleywright@iMac.local>
2024-10-03 20:21:05 -0400

    added a PaymentForm.js file and added the logic to it

---
+++ client/src/components/PaymentForm.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Button, Box } from '@mui/material';
+
+const PaymentForm = ({ onPaymentSucces }) => {
+    const stripe = useStripe();
+    const elements = useElements();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const cardElement = elements.getElement(CardElement);
+
+        const { error, paymentMethod } = await stripe.createPaymentMethod({
+            type: 'card',
+            card: cardElement,
+        });
+
+        if (error) {
+            console.error(error);
+            alert(error.message);
+        } else {
+            console.log('Payment Method:', paymentMethod);
+            onPaymentSucces(paymentMethod);
+        }
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <CardElement />
+            <Box mt={2}>
+                <Button type="submit" variant="contained" color="primary" disabled={!stripe}>
+                    Buy Coffee for ${coffeePrice / 100}
+                </Button>
+            </Box>
+        </form>
+    );
+};
