bea508a1e008433798327123a4f22e1c664bdc2e
valyastriz <valyastriz@gmail.com>
2024-10-08 19:04:40 -0400

    update footer color to match the navbar and header

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,17 +1,19 @@
 // import { useQuery } from '@apollo/client';
 import React from 'react';
-import { Box, Typography } from '@mui/material'
+import { Box, Typography } from '@mui/material';
+import { useTheme } from '@mui/material/styles';
 
 // import { QUERY_PROFILES } from '../utils/queries';
 
 const Footer = () => {
 //   const { loading, data } = useQuery(QUERY_PROFILES);
 //   const profiles = data?.profiles || [];
+const theme = useTheme();
 
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.main',
+        backgroundColor: theme.background.highlight,
         color: 'white',
         textAlign: 'center',
         padding: 2,
