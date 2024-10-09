3dc4f4182df730fb860f15aec908eb8f4d623dc2
defrak2 <defranciscok22@gmail.com>
2024-10-09 19:42:10 -0400

    updated from merge

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import { Box, Typography} from '@mui/material';
 import { useTheme } from '@mui/material/styles';
 
 const About = () => {
