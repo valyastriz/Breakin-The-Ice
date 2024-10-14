e491c4ca7d8598a391681b8d9c1843bb947af3b0
valyastriz <valyastriz@gmail.com>
2024-10-13 20:22:21 -0400

    added vite env variables

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// Load environment variables
+const isProduction = process.env.NODE_ENV === 'production';
 
 export default defineConfig({
     plugins: [react()],
     server: {
-      port: 3000,
-      open: true,
-      proxy: {
-        '/graphql': {
-          target: 'http://localhost:3001',
-          changeOrigin: true,
-          secure: false,
-        },
-        '/create-checkout-session': {
-          target: 'http://localhost:3001',
-          changeOrigin: true,
-          secure: false,
+        port: 3000,
+        open: true,
+        proxy: {
+            '/graphql': {
+                target: isProduction ? 'https://icebreakerstation.onrender.com/graphql' : 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+            },
+            '/create-checkout-session': {
+                target: isProduction ? 'https://icebreakerstation.onrender.com/create-checkout-session' : 'http://localhost:3001',
+                changeOrigin: true,
+                secure: false,
+            }
         }
-      }
     }
-  })
+});
