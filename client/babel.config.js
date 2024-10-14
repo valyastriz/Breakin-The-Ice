72cceb570cd5adcfe1caee14af08f6925f9aeb8b
valyastriz <valyastriz@gmail.com>
2024-10-13 20:17:18 -0400

    removed babel

--- client/babel.config.js
+++
@@ -1,8 +0,0 @@
-// babel.config.js
-module.exports = {
-    presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: [
-      '@babel/plugin-proposal-private-methods',
-      '@babel/plugin-proposal-private-property-in-object',
-    ],
-  };
