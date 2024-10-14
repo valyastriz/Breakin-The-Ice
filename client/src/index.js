120405e887dd2ad29bfab27a684e7f4a2e62c316
valyastriz <valyastriz@gmail.com>
2024-10-13 20:37:36 -0400

    removed service worker

--- client/src/index.js
+++ client/src/index.js
@@ -53,15 +53,15 @@ root.render(
 reportWebVitals();
 
 // Register the service worker
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker
-      .register(`${process.env.PUBLIC_URL}/service-worker.js`)
-      .then((registration) => {
-        console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      })
-      .catch((error) => {
-        console.log('ServiceWorker registration failed: ', error);
-      });
-  });
-}
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker
+//       .register(`${process.env.PUBLIC_URL}/service-worker.js`)
+//       .then((registration) => {
+//         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+//       })
+//       .catch((error) => {
+//         console.log('ServiceWorker registration failed: ', error);
+//       });
+//   });
+// }
