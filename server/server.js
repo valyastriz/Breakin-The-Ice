1c6f8d7c04d7bd700f3a1e3080138fbe0e0ac5eb
valyastriz <valyastriz@gmail.com>
2024-10-16 19:45:00 -0400

    added url for stripe pages

--- server/server.js
+++ server/server.js
@@ -58,8 +58,8 @@ const startApolloServer = async () => {
         },
       ],
       mode: 'payment',
-      success_url: `${YOUR_DOMAIN}success`,
-      cancel_url: `${YOUR_DOMAIN}buyusacoffee`,
+      success_url: `https://icebreakerstation.onrender.com/success`,
+      cancel_url: `https://icebreakerstation.onrender.com/buyusacoffee`,
     });
     console.log(session);
     res.redirect(303, session.url);
